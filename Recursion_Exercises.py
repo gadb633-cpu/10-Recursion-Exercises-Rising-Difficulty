c377e9e9e48063b3c459333c42cf74067e1fb98d
gad-biton <gadb633@gmail.com>
2026-07-06 14:49:07 +0300

    added 2

--- Recursion_Exercises.py
+++ Recursion_Exercises.py
@@ -6,3 +6,11 @@ def power(base, exponent):
     exponent -=1
     return base* power(base, exponent)
 print(power(2,4))
+
+# 2. Factorial
+def factorial(n):
+    if n ==0:
+        return 1
+    
+    return n*factorial(n-1)
+print(factorial(5))
